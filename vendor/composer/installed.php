0eb8807a02d95ffc6e655225b8e60c4577e68083
mahbub500 <mahbubmr500@gmail.com>
2024-09-08 19:21:07 +0600

    chagen pakage name

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => 'mahbub/plugin',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
+        'reference' => '9b51869cff9ed7b256527a905df678b45d9fd193',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'e7fedb7f0ab271cf94f20a6b14939fba0a46163f',
-        'name' => 'mahbub/plugin',
         'dev' => true,
     ),
     'versions' => array(
         'mahbub/plugin' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
+            'reference' => '9b51869cff9ed7b256527a905df678b45d9fd193',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'e7fedb7f0ab271cf94f20a6b14939fba0a46163f',
             'dev_requirement' => false,
         ),
     ),
